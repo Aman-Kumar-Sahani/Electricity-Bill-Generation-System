9b58deb10971b0d6a998e54e77da81870c220723
unknown <Aman7548>
2020-12-26 00:43:10 +0530

    first commit

---
+++ ElectricityBillingSystem-master/ebill/user/footer.php
@@ -0,0 +1,8 @@
+<!-- FOOTER -->
+    <footer class="text-center">
+        <div class="footer-below">
+            <div class="container">
+                Created by Aman Kumar Sahani , Project Based on DataBase Management System
+            </div>
+        </div>
+    </footer>
